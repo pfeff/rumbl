74f3dea7989862e3295aefef2ff60a4df417b03b
Matthew Pfefferle <matt@matt.pfefferle.us>
2019-04-27 17:41:32 -0400

    Init rumble

---
+++ lib/rumbl/accounts.ex
@@ -0,0 +1,25 @@
+defmodule Rumbl.Accounts do
+  @moduledoc """
+  The accounts context
+  """
+
+  alias Rumbl.Accounts.User
+
+  def list_users do
+    [
+      %User{id: 1, name: "Brendan", username: "monkey"},
+      %User{id: 2, name: "Drew", username: "bunny"},
+      %User{id: 3, name: "Michelle", username: "pigtails"}
+    ]
+  end
+
+  def get_user(id) do
+    Enum.find(list_users(), fn map -> map.id == id end)
+  end
+
+  def get_user_by(params) do
+    Enum.find(list_users(), fn map ->
+      Enum.all?(params, fn {key, val} -> Map.get(map, key) == val end)
+    end)
+  end
+end
